87de19ca8889349d80038575885d9709903635fc
srishti77 <=>
2018-03-01 16:47:40 +0100

    Changed CanCut

--- pizzacut.py
+++ pizzacut.py
@@ -1,7 +1,7 @@
 
 pizza = [[0,0,0,1],[1,1,0,0]]
 #slice coordinates
-slice = [[0,0],[1,2]]
+slice = [[0,0],[0,1]]
 
 
 def pizzaCutting(pizza, slice):
@@ -17,14 +17,17 @@ def canCut(pizza, slice, minNoIngredient, maxCells):
     for item in range(slice[0][0], slice[1][0] + 1):
         for item2 in range(slice[0][1], slice[1][1] + 1):
             countCells +=1
-            if  pizza[item][item2] == 0:
-                countTomato += 1
-            if pizza[item][item2] == 1:
-                countMushroom += 1
+            if(pizza[item][item2] == -1):
+                return False
+            else:
+                 if  pizza[item][item2] == 0:
+                    countTomato += 1
+                 if pizza[item][item2] == 1:
+                    countMushroom += 1
+
     if countTomato >= minNoIngredient and countMushroom >= minNoIngredient and countCells <= maxCells:
         return True
     else:
         return False
 
-#print(pizzaCutting(pizza, slice))
-print(canCut(pizza,slice,3, 6))
+
