ea61f592b892a86252ded660680de58393607430
Filip <rusakfilip@gmail.com>
2018-03-01 19:48:56 +0100

    car calss

---
+++ car.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 19:41:25 2018
+
+@author: Filip
+"""
+
+class Car(object):
+    def __init__(self, current_row, current_column, is_busy, car_id, newxt_finish_time):
+        self.current_row = current_row
+        self.current_column = current_column
+        self.is_busy = is_busy
+        self.car_id = car_id
+        self.newxt_finish_time = newxt_finish_time
