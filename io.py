588940b469ed49825c57fd9bb75818c6cc6eb4b9
hlibbabii <hlibbabii@gmail.com>
2018-03-01 13:46:49 +0100

    move io operations to a separate file

---
+++ io.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def read_task(file):
+    data = dict()
+    with open(file, 'r') as f:
+        line = f.readline()
+        split_line = line.split(' ')
+        data['rows'] = int(split_line[0])
+        data['columns'] = int(split_line[1])
+        data['L'] = int(split_line[2])
+        data['H'] = int(split_line[3])
+        matrix = np.zeros(shape=(data['rows'],data['columns']))
+        for i in range(data['rows']):
+            line = f.readline()
+            for j in range(len(line)):
+                if line[j] == 'T':
+                    matrix[i][j] = 1
+
+        data['pizza'] = matrix
+        return data
+
+
+def write_solution(solution, output_file_name):
+    with open(output_file_name, 'w') as f:
+        f.write(str(solution['n']) + '\n')
+        for row in solution['pieces']:
+            f.write(' '.join(str(elm) for elm in row) + '\n')
--- main.py
+++ main.py
@@ -1,26 +1,8 @@
+import io
+
 __author__ = 'hlib'
 
-import numpy as np
 import pprint as pp
-
-def read_task(file):
-    data = dict()
-    with open(file, 'r') as f:
-        line = f.readline()
-        split_line = line.split(' ')
-        data['rows'] = int(split_line[0])
-        data['columns'] = int(split_line[1])
-        data['L'] = int(split_line[2])
-        data['H'] = int(split_line[3])
-        matrix = np.zeros(shape=(data['rows'],data['columns']))
-        for i in range(data['rows']):
-            line = f.readline()
-            for j in range(len(line)):
-                if line[j] == 'T':
-                    matrix[i][j] = 1
-
-        data['pizza'] = matrix
-        return data
 
 
 def solve(data):
@@ -30,18 +12,10 @@ def solve(data):
     return solution
 
 
-def write_solution(solution, output_file_name):
-    with open(output_file_name, 'w') as f:
-        f.write(str(solution['n']) + '\n')
-        for row in solution['pieces']:
-            f.write(' '.join(str(elm) for elm in row) + '\n')
-
-
-
 def main(input_file, output_file):
-    data = read_task(input_file)
+    data = io.read_task(input_file)
     pp.pprint(data)
     solution = solve(data)
-    write_solution(solution, output_file)
+    io.write_solution(solution, output_file)
 
 main('Inputs/example.in', 'Outputs/example.out')
