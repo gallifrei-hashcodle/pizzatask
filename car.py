b808546ce3a6475c300024a4d90d9582b3f894b5
hlibbabii <hlibbabii@gmail.com>
2018-03-01 20:23:48 +0100

    update car

--- car.py
+++ car.py
@@ -12,3 +12,14 @@ class Car(object):
         self.is_busy = is_busy
         self.car_id = car_id
         self.newxt_finish_time = newxt_finish_time
+        self.assigned_rides = []
+
+    def assign_ride(self, ride, start_time):
+        self.assigned_rides.append(ride.ride_id)
+        self.current_row = ride.end_row
+        self.current_column = ride.end_column
+        self.newxt_finish_time = start_time + ride.duration
+
+    def is_busy(self, time):
+        return time < self.newxt_finish_time
+
