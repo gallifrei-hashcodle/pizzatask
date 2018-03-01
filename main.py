9765a543cb11d5b844fc68b13480b9900c33bfb7
hlibbabii <hlibbabii@gmail.com>
2018-03-01 20:01:05 +0100

    add

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import in_out
 
-data = in_out.read_task('Inputs/example.in')
+
+def sortRidesByStartTime(all_rides):
+    pass
+
+
+def get_car_with_least_waiting_time(ride):
+    pass
+
+
+def solve():
+    data = in_out.read_task('Inputs/example.in')
+    all_rides = data['ride_list']
+    sortRidesByStartTime(all_rides)
+    total_time = data['steps']
+    current_time = 0
+    pending_rides = all_rides
+    while current_time < total_time and len(pending_rides) > 0:
+        for ride in all_rides:
+            if ride in pending_rides:
+                assigned_car = get_car_with_least_waiting_time(ride)
+                assigned_car.assignRide(ride)
+                pending_rides.remove(ride)
+
+
 
