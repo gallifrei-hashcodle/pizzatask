7e535d9f9cc97fa1ba09a1d4afb596e17c1af409
hlibbabii <hlibbabii@gmail.com>
2018-03-01 16:24:35 +0100

    add slice class

---
+++ slice.py
@@ -0,0 +1,32 @@
+def basic_slice(size, start_row, start_column):
+    return Slice(start_row, start_column, start_row, start_column + size)
+
+class Slice(object):
+    def __init__(self, up_row, up_column, down_row, down_column):
+        self.up_row = up_row
+        self.up_column = up_column
+        self.down_row = down_row
+        self.down_column = down_column
+
+    def size(self):
+        return self.rows() * self.columns()
+
+    def rows(self):
+        return self.down_row - self.up_row + 1
+
+    def columns(self):
+        return self.down_column - self.up_column + 1
+
+    def transform(self):
+        size = self.size()
+        if self.rows() / self.columns() == size and self.rows() % self.columns() == 0:
+            return basic_slice(++size, self.up_row, self.up_column)
+        else:
+            new_rows = self.rows() + 1
+            while size % new_rows != 0 and new_rows < size:
+                new_rows += 1
+            new_columns = size / new_rows
+            return Slice(self.up_row, self.up_column, self.up_row + new_rows - 1, self.up_column + new_columns - 1)
+
+    def __str__(self):
+        return str([self.up_row, self.up_column, self.down_row, self.down_column])
